da5a0bb013888faa3a8b8531ecd7424806217797
Emilia <emiliaprzybylek@hotmail.com>
2021-11-12 15:26:38 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+'use strict';
+
+function validate(e) {
+    e.preventDefault();
+
+    const name = this.name.value;
+    const mail = this.mail.value;
+    const mail2 = this.mail2.value;
+    const city = this.city.selectedIndex;
+    const rodo = this.rodo.checked;
+
+    const placeError = document.querySelector('#errors');
+    placeError.innerHTML = '';
+
+    let errors = [];
+
+    const namePattern = /^[A-ZŁŚĆŻ]+[a-złśćżź]{2,20}$/;
+    const testName = namePattern.test(name);
+    if(!testName) 
+        errors.push('Imię niepoprawne');
+    
+        
+    const mailPattern = /^[a-z\d]+[\w.-]*@[a-z\d]+[a-z\d-]*\.[a-z]{2,4}$/i;
+    const testMail = mailPattern.test(mail);
+    if(!testMail)
+        errors.push('Niepoprawny format e-mail');
+
+    if(mail != mail2)
+        errors.push('Maile różnią się od siebie');
+
+    if(!city)
+        errors.push('Wybierz miasto');
+
+    if(!rodo)
+        errors.push('Zaakceptuj RODO')
+
+    if(errors.length > 0) {
+        placeError.innerHTML = errors.join('<br>');
+    } 
+
+    console.log(errors);
+    } 
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', validate);
